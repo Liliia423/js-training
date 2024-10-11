e09d909fc53728003aede4fbc94c17c7bfcea405
Liliia423 <etude4408@gmail.com>
2024-10-11 10:07:37 +0300

    rty

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,9 @@
+'use strict';
+
 function getElementWidth(content, padding, border) {
-  console.log(Number.parseFloat('50px'));
-  console.log(Number.parseFloat('8px'));
-  console.log(Number.parseFloat('4px'));
-  console.log(Number.parseFloat('60px'));
-  console.log(Number.parseFloat('12px'));
-  console.log(Number.parseFloat('8.5px'));
-  console.log(Number.parseFloat('200px'));
-  console.log(Number.parseFloat('0px'));
-  console.log(Number.parseFloat('0px'));
-  console.log(`${(padding + border) * 2}) + ${content}`);
+  const totalWidth = content + (padding + border) * 2;
+  console.log(`${totalWidth}`);
 }
-console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth(50, 8, 4));
 console.log(getElementWidth('60px', '12px', '8.5px'));
 console.log(getElementWidth('200px', '0px', '0px'));
