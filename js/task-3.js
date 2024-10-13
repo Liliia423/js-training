e28d4e99e4cd58505da0939b79be238cc03a34ac
Liliia423 <etude4408@gmail.com>
2024-10-13 20:55:47 +0300

    rty

--- js/task-3.js
+++ js/task-3.js
@@ -1,64 +1,43 @@
 'use strict';
-function getElementWidth(content, padding, border) {
-  console.log(typeof content); //number
-  console.log(typeof padding); //number
-  console.log(typeof border); //number
-  let results = content + (padding * 2 + border * 2);
-  console.log(typeof results); //number
-  return results;
-}
-console.log(getElementWidth(60, 12, 8.5)); //101
-
 /*function getElementWidth(content, padding, border) {
-  console.log(typeof content); //string
-  console.log(typeof padding); //number
-  console.log(typeof border); //number
-  let results = content + (padding * 2 + border * 2);
-  console.log(typeof results); //string
-  return results;
-}
-console.log(getElementWidth('60px', 12, 8.5)); //60px41*/
+  //let num = new Number(content, padding, border);
 
-/*function getElementWidth(content, padding, border) {
-  console.log(typeof content); //string
-  console.log(typeof padding); //string
-  padding *=1;
-  console.log(typeof padding);//number
-  console.log(typeof border); //number
-  let results = content + (padding * 2 + border * 2);
-  console.log(typeof results); //string
-  return results;
-}
-console.log(getElementWidth('60px', '12px', 8.5)); //60pxNaN*/
-
-/*function getElementWidth(content, padding, border) {
-  console.log(typeof content); //string
-  console.log(typeof padding); //string
-  console.log(typeof border); //string
-  let results = content + (padding * 2 + border * 2);
-  console.log(typeof results); //string
-  return results;
-}
-console.log(getElementWidth('60px', '12px', '8.5px')); //60pxNaN*/
-
-/*function getElementWidth(content, padding, border) {
-  console.log(typeof content); //string
-  console.log(typeof padding); //string
-  console.log(typeof border); //string
-  let results = content + padding * 2 + border * 2;
-  console.log(typeof results); //string
-  return results;
-}
-console.log(getElementWidth('60px', '12px', '8.5px'));*/
-
-/*function getElementWidth(content, padding, border) {
-  console.log(typeof content); //string
-  console.log(typeof padding); //string
-  console.log(typeof border); //string
-  border *= 1;
+  console.log(typeof content);
+  console.log(typeof padding);
   console.log(typeof border);
-  let results = content + padding + border;
-  console.log(typeof results); //string
-  return results;
+  return content - 0 + padding * 2 + border * 2;
 }
-console.log(getElementWidth('60px', '12px', '8.5px'));*/
+
+console.log(getElementWidth('50px', '8px', '4px'));
+isNaN(getElementWidth);
+//console.log(getElementWidth('50px', '8px', '4px'));
+//console.log(getElementWidth('60px', '12px', '8.5px'));
+//console.log(getElementWidth('200px', '0px', '0px'));
+//console.log(getElementWidth(50, 8, 4));*/
+
+function getElementWidth(content, padding, border) {
+  console.log(content.length);
+
+  console.log(content.indexOf('p')); // position in string = 2
+  console.log(content.indexOf('x'));
+
+  console.log(content.endsWith('px'));
+  let lastLastSymbolContent = content[content.length - 2];
+  console.log(lastLastSymbolContent);
+  let lastSymbolContent = content[content.length - 1];
+
+  console.log(lastSymbolContent);
+
+  return content + padding + padding + border + border;
+}
+
+console.log(getElementWidth('200px', '0px', '0px'));
+console.log(getElementWidth('50px', '8px', '4px'));
+
+//console.log(getElementWidth('60px', '12px', '8.5px'));
+//
+
+//isNaN(getElementWidth);
+//console.log(typeof content);
+
+//
