888019f8f9ebdef7dc32512d4b413e3992e67b48
Liliia423 <etude4408@gmail.com>
2024-10-13 16:43:05 +0300

    rty

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,64 @@
 'use strict';
 function getElementWidth(content, padding, border) {
-  console.log((content += padding * 2));
-
-  content = '50px';
-
-  padding = '8px';
-
-  border = '4px';
-
-  console.log(content + padding * 2 + border * 2);
-
-  //зробити розрахунок і повернути значення у пікселях
+  console.log(typeof content); //number
+  console.log(typeof padding); //number
+  console.log(typeof border); //number
+  let results = content + (padding * 2 + border * 2);
+  console.log(typeof results); //number
+  return results;
 }
-console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth(60, 12, 8.5)); //101
+
+/*function getElementWidth(content, padding, border) {
+  console.log(typeof content); //string
+  console.log(typeof padding); //number
+  console.log(typeof border); //number
+  let results = content + (padding * 2 + border * 2);
+  console.log(typeof results); //string
+  return results;
+}
+console.log(getElementWidth('60px', 12, 8.5)); //60px41*/
+
+/*function getElementWidth(content, padding, border) {
+  console.log(typeof content); //string
+  console.log(typeof padding); //string
+  padding *=1;
+  console.log(typeof padding);//number
+  console.log(typeof border); //number
+  let results = content + (padding * 2 + border * 2);
+  console.log(typeof results); //string
+  return results;
+}
+console.log(getElementWidth('60px', '12px', 8.5)); //60pxNaN*/
+
+/*function getElementWidth(content, padding, border) {
+  console.log(typeof content); //string
+  console.log(typeof padding); //string
+  console.log(typeof border); //string
+  let results = content + (padding * 2 + border * 2);
+  console.log(typeof results); //string
+  return results;
+}
+console.log(getElementWidth('60px', '12px', '8.5px')); //60pxNaN*/
+
+/*function getElementWidth(content, padding, border) {
+  console.log(typeof content); //string
+  console.log(typeof padding); //string
+  console.log(typeof border); //string
+  let results = content + padding * 2 + border * 2;
+  console.log(typeof results); //string
+  return results;
+}
+console.log(getElementWidth('60px', '12px', '8.5px'));*/
+
+/*function getElementWidth(content, padding, border) {
+  console.log(typeof content); //string
+  console.log(typeof padding); //string
+  console.log(typeof border); //string
+  border *= 1;
+  console.log(typeof border);
+  let results = content + padding + border;
+  console.log(typeof results); //string
+  return results;
+}
+console.log(getElementWidth('60px', '12px', '8.5px'));*/
