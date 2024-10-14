d1c5030d1e07a7fb350732ba11c2a5464e127ebc
Liliia423 <etude4408@gmail.com>
2024-10-14 08:37:03 +0300

    tyu

--- js/task-3.js
+++ js/task-3.js
@@ -1,43 +1,41 @@
 'use strict';
-/*function getElementWidth(content, padding, border) {
-  //let num = new Number(content, padding, border);
-
-  console.log(typeof content);
-  console.log(typeof padding);
-  console.log(typeof border);
-  return content - 0 + padding * 2 + border * 2;
-}
-
-console.log(getElementWidth('50px', '8px', '4px'));
-isNaN(getElementWidth);
-//console.log(getElementWidth('50px', '8px', '4px'));
-//console.log(getElementWidth('60px', '12px', '8.5px'));
-//console.log(getElementWidth('200px', '0px', '0px'));
-//console.log(getElementWidth(50, 8, 4));*/
-
 function getElementWidth(content, padding, border) {
-  console.log(content.length);
+  let newContent = content.slice(0, -2);
+  let newPadding = padding.slice(0, -2);
+  let newBorder = border.slice(0, -2);
 
-  console.log(content.indexOf('p')); // position in string = 2
-  console.log(content.indexOf('x'));
+  newContent = Number(newContent);
+  //console.log(typeof newContent);
 
-  console.log(content.endsWith('px'));
-  let lastLastSymbolContent = content[content.length - 2];
-  console.log(lastLastSymbolContent);
-  let lastSymbolContent = content[content.length - 1];
+  newPadding = Number(newPadding);
+  //console.log(typeof newPadding);
 
-  console.log(lastSymbolContent);
+  newBorder = Number(newBorder);
+  //console.log(typeof newBorder);
 
-  return content + padding + padding + border + border;
+  return newContent + newPadding * 2 + newBorder * 2;
 }
 
-console.log(getElementWidth('200px', '0px', '0px'));
 console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
 
-//console.log(getElementWidth('60px', '12px', '8.5px'));
-//
+//======================================================
+//console.log(newContent.length); // undefined - розібратись чому?
+//======================================================
 
 //isNaN(getElementWidth);
 //console.log(typeof content);
 
-//
+//console.log(content.length);
+//console.log(content.indexOf('p'));
+//console.log(content.indexOf('x'));
+
+//console.log(content.endsWith('px'));
+
+//let lastLastSymbolContent = content[content.length - 2];
+//console.log(lastLastSymbolContent);
+//let lastSymbolContent = content[content.length - 1];
+
+//console.log(lastSymbolContent);
+//let resultContent = content * 2;
