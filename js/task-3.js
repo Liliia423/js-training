9016f678d9416a501da67324c88e482c95dcf520
Liliia423 <etude4408@gmail.com>
2024-10-20 12:36:16 +0300

    tyu

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,18 @@
 'use strict';
+function checkForSpam(message) {
+  let newMessage = message.toLowerCase();
+  return newMessage.includes('spam') || newMessage.includes('sale');
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+/*homework-01-variant-1
 function getElementWidth(content, padding, border) {
   // Перетворюємо рядки на числа, parseFloat() ігнорує "px"
   let newContent = parseFloat(content);
@@ -10,10 +24,11 @@ function getElementWidth(content, padding, border) {
 
 console.log(getElementWidth('50px', '8px', '4px'));
 console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(getElementWidth('200px', '0px', '0px'));*/
 
 //======================================================
-/*function getElementWidth(content, padding, border) {
+/*homework-01-variant-2
+function getElementWidth(content, padding, border) {
   let newContent = content.slice(0, -2);
   let newPadding = padding.slice(0, -2);
   let newBorder = border.slice(0, -2);
