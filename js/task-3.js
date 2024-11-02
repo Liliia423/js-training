74929f2cc7f6c816714f1a02fc16e122a52f6d23
Liliia423 <etude4408@gmail.com>
2024-11-02 07:24:25 +0200

    rty

--- js/task-3.js
+++ js/task-3.js
@@ -13,17 +13,34 @@ console.log(filterArray([1, 2, 3, 4, 5], 5));
 console.log(filterArray([12, 24, 8, 41, 76], 38));
 console.log(filterArray([12, 24, 8, 41, 76], 20));*/
 
-function filterArray(numbers, value) {
+/*function filterArray(numbers, value) {
   let newFilterArray = [];
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers > value) {
-    }
+  for (let number of numbers) {
+    newFilterArray.push(number);
   }
-  return newFilterArray.push(...numbers);
+  return newFilterArray;
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(filterArray([1, 2, 3, 4, 5], 3));*/
+
+function filterArray(numbers, value) {
+  let newFilterArray = [];
+
+  for (let number of numbers) {
+    if (number > value) {
+      newFilterArray.push(number);
+    }
+  }
+  return newFilterArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+/*оптимізовано ChatGPT 4o with canvas
+function filterArray(numbers, value) {
+  return numbers.filter(number => number > value);
+}*/
